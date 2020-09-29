edf1d962a70836c85852bdfaaa2010eead11acdc
aynur <galimovaynur@protonmail.com>
2020-09-30 00:06:10 +0500

    0.2. Кидаем исключение - throw new RuntimeException [#357353]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,19 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        System.out.println(new Fact().calc(5));
+        System.out.println(new Fact().calc(-5));
+    }
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Argument n should be greater or equal zero.");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
