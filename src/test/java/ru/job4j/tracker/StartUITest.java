9618a23fe1ec0a1bcf7c8a1acdaef7a3341e0f1a
aynur <galimovaynur@protonmail.com>
2020-10-02 21:29:41 +0500

    1.1. Тесты на StartUI. [#357358]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -122,4 +122,26 @@ public class StartUITest {
                 + "0. Find items by name" + System.lineSeparator()
                 + "1. Exit" + System.lineSeparator()));
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] { "-42", "0" }
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. Exit%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. Exit%n"
+                )
+        ));
+    }
 }
