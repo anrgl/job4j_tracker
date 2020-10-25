053ca287d4944b20c2cb5062008246b02a05417b
aynur <galimovaynur@protonmail.com>
2020-10-25 22:46:06 +0500

    2. Local-Variable Type Inference [#357160]

--- src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -9,28 +9,28 @@ import static org.hamcrest.Matchers.is;
 public class PriorityQueueTest {
     @Test
     public void whenHigherPriority() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("middle", 3));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDesc(), is("urgent"));
     }
 
     @Test
     public void whenAllEqualsPriority() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("Alpha", 42));
         queue.put(new Task("Beta", 42));
         queue.put(new Task("Gamma", 42));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDesc(), is("Gamma"));
     }
 
     @Test
     public void whenNoTasks() {
-        PriorityQueue queue = new PriorityQueue();
-        Task result = queue.take();
+        var queue = new PriorityQueue();
+        var result = queue.take();
         assertThat(result, nullValue());
     }
 }
