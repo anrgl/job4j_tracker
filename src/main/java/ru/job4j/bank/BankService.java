580c438cb20ccaa1dac05732c82ca37243438a5f
aynur <galimovaynur@protonmail.com>
2020-11-01 19:44:53 +0500

    4. Optional в Stream API. [#357162] v2

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -39,11 +39,11 @@ public class BankService {
         boolean rsl = false;
         Optional<Account> srcAccOpt = findByRequisite(srcPassport, srcRequisite);
         Optional<Account> destAccOpt = findByRequisite(destPassport, destRequisite);
-        Account src = srcAccOpt.get();
-        Account dest = destAccOpt.get();
-        if (src.getBalance() - amount >= 0) {
-            src.setBalance(src.getBalance() - amount);
-            dest.setBalance(dest.getBalance() + amount);
+        if (srcAccOpt.isPresent()
+                && destAccOpt.isPresent()
+                && srcAccOpt.get().getBalance() - amount >= 0) {
+            srcAccOpt.get().setBalance(srcAccOpt.get().getBalance() - amount);
+            destAccOpt.get().setBalance(destAccOpt.get().getBalance() + amount);
             rsl = true;
         }
         return rsl;
