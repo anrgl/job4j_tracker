1ca930d67b1de865aec236c5da586ef09e56c3fa
aynur <galimovaynur@protonmail.com>
2021-01-10 21:20:53 +0500

    1. Sql схема для проекта Tracker. [#419994]

---
+++ db/insert.sql
@@ -0,0 +1,9 @@
+insert into items(name) values ('Item #1');
+insert into items(name) values ('Item #2');
+insert into items(name) values ('Item #3');
+insert into items(name) values ('Item #4');
+insert into items(name) values ('Item #5');
+insert into items(name) values ('Item #6');
+insert into items(name) values ('Item #7');
+insert into items(name) values ('Item #8');
+insert into items(name) values ('Item #9');
