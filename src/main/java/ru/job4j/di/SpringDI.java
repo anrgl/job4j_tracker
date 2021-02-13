3c410dcc3d6b0314c567a30ada7a0b547aebfefd
Aynur Galimov <galimovaynur@protonmail.com>
2021-02-13 23:43:06 +0500

    4. Время жизни (Scope) [#450462]

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -7,10 +7,10 @@ public class SpringDI {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.scan("ru.job4j.di");
         context.refresh();
-        StartUI ui = context.getBean(StartUI.class);
-        ui.add("Petr Arsentev");
-        ui.add("Fedor Fedorovich");
-        ui.addFromConsoleInput();
-        ui.print();
+        Store store = context.getBean(Store.class);
+        store.add("Petr Arsentev");
+        store.add("Fedor Fedorovich");
+        Store another = context.getBean(Store.class);
+        another.getAll().forEach(System.out::println);
     }
 }
